1f2f4ceb6b84c7ea11c8a84f63fa7cb6b5d328b8
huncode <me@huncode.com>
2015-11-26 15:54:52 +0300

    Add byRegion filter method

--- src/outlets.factory.js
+++ src/outlets.factory.js
@@ -3,8 +3,21 @@ export default /*@ngInject*/ function ($http) {
 
   factory.fetch = function () {
     return $http.get('http://api.love.sl/v2/outlets/').then(function (response) {
+      factory.all = response.data;
       return response.data;
     });
+  };
+
+  factory.byRegion = function (id) {
+    if (!factory._byRegion) {
+      factory._byRegion = {};
+    }
+
+    if (!factory._byRegion[id]) {
+      factory._byRegion[id] = factory.all.filter((outlet) => outlet.region_id.indexOf(id) !== -1);
+    }
+
+    return factory._byRegion[id];
   };
 
   return factory;
